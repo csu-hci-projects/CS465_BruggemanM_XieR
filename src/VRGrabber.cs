44a842307ed11030ac6e0ce90aa0fa181f360889
Molly Bruggeman <mollymbb@colostate.edu>
2025-03-14 14:18:14 -0600

    Update VRGrabber.cs

--- src/VRGrabber.cs
+++ src/VRGrabber.cs
@@ -10,3 +10,7 @@ public class VRGrabber : MonoBehaviour
         grabInteractable = GetComponent<XRGrabInteractable>();
     }
 }
+
+//Makes an object grabable in VR by using XRGrabInteractable which is the built-in Unity XR command for handling VR Interactions. 
+//It then automatically assigns the XRGrabInteractable element right when the script starts.
+//Attach this script to any VR object and it will make it grabbable. 
